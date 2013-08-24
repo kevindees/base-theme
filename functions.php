30d5a8f5216aca4b9b5af59fcdb672004608f384
Kevin Dees <kevindees@Kevins-MacBook-Pro.local>
2013-08-23 21:09:18 -0400

    add composer for ACPT and title fixes

--- functions.php
+++ functions.php
@@ -3,6 +3,11 @@
 define('TEMP', get_template_directory_uri());
 define('TEMP_CHILD', get_stylesheet_directory_uri());
 
+// RUNNING COMPOSER?
+// In the acpt package, once downloaded. Make set the config.php path
+// for ACPT_FOLDER_NAME to vendor/robojuice/acpt in the framework.
+// require 'vendor/autoload.php';
+
 include('functions/init.php');
 include('functions/js_css.php');
 include('functions/sidebars.php');
--- functions/init.php
+++ functions/init.php
@@ -7,3 +7,22 @@ add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'post-thumbnails' );
 
 remove_action('wp_head', 'wp_generator');
+
+function base_wp_title( $title, $sep ) {
+	global $paged, $page;
+
+	if ( is_feed() )
+		return $title;
+
+	$title .= get_bloginfo( 'name' );
+
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title = "$title $sep $site_description";
+
+	if ( $paged >= 2 || $page >= 2 )
+		$title = "$title $sep " . sprintf('Page %s', max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', 'base_wp_title', 10, 2 );
