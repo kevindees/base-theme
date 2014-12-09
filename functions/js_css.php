a1b2390880756a7b0419dd794254f791f90d0ef6
Kevin Dees <kevin@robojuice.com>
2014-12-09 10:09:02 -0500

    fix constant var

--- functions/js_css.php
+++ functions/js_css.php
@@ -6,22 +6,22 @@ function add_scripts_styles() {
 
 	if(!is_admin()) :
 		// JS
-		wp_register_script( 'modernizr', TEMP . '/js/libs/modernizr.min.js', array(), '1.0' );
+		wp_register_script( 'modernizr', THEME_DIR . '/js/libs/modernizr.min.js', array(), '1.0' );
 		wp_enqueue_script( 'modernizr' );
 		wp_enqueue_script("jquery");
 
-		wp_register_script( 'main-plugins', TEMP . '/js/plugins.js', array('jquery'), '1.0', true );
+		wp_register_script( 'main-plugins', THEME_DIR . '/js/plugins.js', array('jquery'), '1.0', true );
 		wp_enqueue_script( 'main-plugins' );
 
-		wp_register_script( 'main-script', TEMP . '/js/script.js', array('jquery'), '1.0', true );
+		wp_register_script( 'main-script', THEME_DIR . '/js/script.js', array('jquery'), '1.0', true );
 		wp_enqueue_script( 'main-script' );
 
 		// CSS
-		wp_enqueue_style( 'main-style', TEMP . '/style.css' );
-		wp_enqueue_style( 'custom-style', TEMP . '/css/style.css' );
+		wp_enqueue_style( 'main-style', THEME_DIR . '/style.css' );
+		wp_enqueue_style( 'custom-style', THEME_DIR . '/css/style.css' );
 
 		// IE CSS
-		//wp_enqueue_style( 'main-ie', get_template_directory_uri() . '/css/ie.css', array( 'main-style' ) );
+		//wp_enqueue_style( 'main-ie', THEME_DIR . '/css/ie.css', array( 'main-style' ) );
 		//$wp_styles->add_data( 'main-ie', 'conditional', 'lt IE 9' );
 
 		// WP
