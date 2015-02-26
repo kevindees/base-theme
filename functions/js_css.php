839869b2d9da6eb42f2900960f41fc46fc94a345
kevindees <kevin@robojuice.com>
2015-02-25 21:44:13 -0500

    fixes from bad commit

--- functions/js_css.php
+++ functions/js_css.php
@@ -2,28 +2,16 @@
 add_action('wp_enqueue_scripts', 'add_scripts_styles');
 
 function add_scripts_styles() {
-	global $wp_styles;
-
 	if(!is_admin()) :
 		// JS
-		wp_register_script( 'modernizr', THEME_DIR . '/js/libs/modernizr.min.js', array(), '1.0' );
-		wp_enqueue_script( 'modernizr' );
+        wp_enqueue_script( 'modernizr', THEME_DIR . '/js/libs/modernizr.min.js', array(), '1.0' ); // shiv and svg
 		wp_enqueue_script("jquery");
-
-		wp_register_script( 'main-plugins', THEME_DIR . '/js/plugins.js', array('jquery'), '1.0', true );
-		wp_enqueue_script( 'main-plugins' );
-
-		wp_register_script( 'main-script', THEME_DIR . '/js/script.js', array('jquery'), '1.0', true );
-		wp_enqueue_script( 'main-script' );
+        wp_enqueue_script( 'main-plugins', THEME_DIR . '/js/plugins.js', array('jquery'), '1.0', true );
+        wp_enqueue_script( 'main-script', THEME_DIR . '/js/script.js', array('jquery'), '1.0', true );
 
 		// CSS
-		wp_enqueue_style( 'comments-style', THEME_DIR . '/css/comments.css' );
 		wp_enqueue_style( 'main-style', THEME_DIR . '/style.css' );
 		wp_enqueue_style( 'custom-style', THEME_DIR . '/css/style.css' );
-
-		// IE CSS
-		//wp_enqueue_style( 'main-ie', THEME_DIR . '/css/ie.css', array( 'main-style' ) );
-		//$wp_styles->add_data( 'main-ie', 'conditional', 'lt IE 9' );
 
 		// WP
 		wp_enqueue_script( 'comment-reply');
--- parts/head.php
+++ parts/head.php
@@ -1,6 +1,5 @@
 <!doctype html>
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if lt IE 9]>      <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
@@ -8,7 +7,5 @@
   <meta name="viewport" content="width=device-width">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <?php wp_head(); ?>
-
 </head>
 <body <?php body_class(); ?>>
-<?php get_template_part( 'chromeframe' ); ?>
